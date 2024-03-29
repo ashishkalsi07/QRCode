22df88aed7474cf5e07876c7f0fc759b4a525623
Ashish Kalsi <68415917+ashishkalsi07@users.noreply.github.com>
2024-03-29 19:49:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import qrcode
-myqr = qrcode.make("https://www.linkedin.com/in/ashishkalsi07/")
+myqr = qrcode.make("https://www.linkedin.com/in/ashishkalsi07/")  # you can paste any link here . A png image will appear here after running code in terminal which is your QR code 
 myqr.save("mqr.png")
